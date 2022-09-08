7116ae0f15d4dc5e3acae1b79a5ddda28fa349e2
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-08 13:24:26 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (success)
- *
- *     */
-int main(void) {
+*
+** main - Entry point
+*
+**
+*
+** Return: Always 0 (success)
+*
+**/
+int main(void) 
+{
 int a;
 long int b;
 long long int c; 
