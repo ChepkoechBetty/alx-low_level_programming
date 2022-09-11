5d14422f65195612ab28aaa0f3aff19621a80f93
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-11 06:36:20 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/*
+ * main - Entry point
+ * Description:prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ *
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
