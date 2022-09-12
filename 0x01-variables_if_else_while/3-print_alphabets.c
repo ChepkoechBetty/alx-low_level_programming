7b5730bcfb931be8ae461a4f584463aa68b6fe5c
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-12 00:15:33 -0700

    main description

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/*
+/**
  * main - Prints out all the alphabets in
  * lowercase and upper case.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/*
+/**
  * main - Prints out all the numbers
  * between 0 and 9.
+ *
  * Return: Always 0 (Success)
 */
 int main(void)
