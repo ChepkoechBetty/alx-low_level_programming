d6d84b3587f65b71d1b742fc891f94588305dda3
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-15 10:52:17 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _abs - find absolute value
+ * @int: integer
+ *
+ * Return: 1
+ */
+int _abs(int)
+{
+	int a;
+
+	a = abs(x);
+	write(1, "a", 1);
+	return (0);
+}
