7d59ea9b0e1c7c5a001aebc189bd04d836cb148b
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-15 14:12:06 -0700

    to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -22,8 +22,15 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			if (i == 98)
+			{
 				printf("%d", i);
-			printf("%d, ", i);
+				printf("\n");
+			}
+			else
+			{
+				printf("%d, ", i);
+				printf("\n");
+			}
 		}
 	}
 	else
