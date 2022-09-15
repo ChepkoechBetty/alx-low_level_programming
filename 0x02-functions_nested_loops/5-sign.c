f9766cd38193ccc6b15fdba232ef04211f4b554c
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-15 10:02:21 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		write(1, '+1', 2);
+		write(1, "+1", 2);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		write(1, '00', 2);
+		write(1, "00", 2);
 		return (0);
 	}
 	else
 	{
-		write(1, '-1', 2);
+		write(1, "-1", 2);
 		return (-1);
 	}
 }
