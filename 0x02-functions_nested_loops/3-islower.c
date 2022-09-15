438b57f0bd668330c263ce8c8e2884a50659196d
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-15 05:48:52 -0700

    islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _islower - Check for lower case character
+ * c:Paremeter
+ *
+ * Return: Always (1) success (0) failure
+ */
+int _islower(int c)
+{
+	if (c.islower)
+		return (1);
+	return (0);
+}
