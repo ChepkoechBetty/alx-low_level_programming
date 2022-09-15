8565f9f6d8bd0297d90d2605a8da69aea2af1547
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-15 09:42:09 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * print_sign - prin signs
+ * @n: parameter
+ *
+ * Return: 1
+ */
+int print_sign(int n)
+{
+	if(n>0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if(n<0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
