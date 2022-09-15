06b53bdd7f71f4f6cd0a962041c8e5842c51de53
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-15 05:19:39 -0700

    alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - print alphabet x10
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int c;
+	int i;
+
+	for (i=1; i<=10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+	}
+	putchar ('\n');
+}
