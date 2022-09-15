1e54f28f050715318d8effbc60c90ec6e5690ea0
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-15 12:36:56 -0700

    9-times_table

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * times_table - print 9-times table
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int start;
+	int m;
+	int r;
+
+	for (start = 0; start <= 9; start++)
+	{
+		for (m = 0; m <= 9; m++)
+		{
+			r = start * m;
+			return (r);
+		}
+	}
+}
