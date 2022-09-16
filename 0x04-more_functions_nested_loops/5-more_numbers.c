73321d624add9e0d9a2adf53a681563cccfa83cf
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-16 08:09:02 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,18 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = '0'; i < '10'; i++)
+	for (j = '0'; j < '10'; j++)
 	{
-		for (j = '0'; j <= '14'; j++)
+		i = 0;
+		while (i <= 14)
 		{
-			if (j >= '10')
+			if (i >= 10)
 			{
 				_putchar(i / 10 + '0');
 			}
 			_putchar(i % 10 + '0');
+			i++;
 		}
-	        _putchar('\n');
+		_putchar('\n');
 	}
 }
