a68f8c0b6e5e543c0cef2c87c2fa0dd680552374
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-16 03:56:19 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 /**
  * print_line - print line
+ * @n:Parameter
  */
 void print_line(int n)
 {
-	for (int i = 1; i <= n; i++)
+	if (n <= 0)
 	{
-		putchar('_');
+		putchar('\n');
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			putchar('_');
+		}
 	}
 	putchar('\n');
 }
