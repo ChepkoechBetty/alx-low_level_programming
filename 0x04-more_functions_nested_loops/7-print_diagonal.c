8afad45c4759070f514992d67ad4d337af2d48e5
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-16 04:14:05 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * print_diagonal - print diagonal line
+ * @n:Parameter
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		int i = 1;
+
+		while (i <= n)
+		{
+			putchar('\');
+			putchar('\n');
+		}
+	}
+}
