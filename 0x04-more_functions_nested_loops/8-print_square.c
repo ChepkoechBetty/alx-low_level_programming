7d0988e69c604e6597ef3314e70085818990b0d4
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-16 07:47:53 -0700

    print-square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,17 +6,20 @@
  */
 void print_square(int size)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i = 1;
+	int i, j;
 
-		while (i <= size)
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+
+				if (i == size - 1)
+					continue;
+				_putchar('\n');
+			}
 		}
 		_putchar('\n');
 	}
