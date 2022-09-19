3066ed81094a504ed5537c5898ba525b78aad93a
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-19 04:32:32 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+/**
+ * swap_int - Swap values of two pointers
+ *
+ * @a:Parameter 1
+ *
+ * @b:Parameter 2
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int a;
+	int b;
+
+	a = *a;
+	b = *b;
+	*a = b;
+	*b = a;
+}
