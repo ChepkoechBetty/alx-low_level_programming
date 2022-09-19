3c68eb0c824682b8f816a9aeca680a286dc58a08
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-19 05:00:31 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen - return length of a string
+ *
+ * @s:Pointer parameter
+ *
+ * Return: int success
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	_putchar(len);
+}
