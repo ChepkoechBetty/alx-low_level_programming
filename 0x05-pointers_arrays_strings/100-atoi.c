ce702ff24255f8968bbdeacae27dc79401d586cb
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-20 05:13:36 -0700

    atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _atoi - atoi function
+ *
+ * @s: Parameter
+ *
+ * Return: 0 success
+ *
+ */
+int _atoi(char *s)
+{
+	int *s = atoi(s);
+
+	Return(0);
+}
