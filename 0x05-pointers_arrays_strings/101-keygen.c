74c2d42bc696a424f6fd45123d69a609278de81f
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-20 06:09:36 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int sum;
+	char d;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		d = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+
+	return (0);
+}
