b5a6bca58df3e64ed552df7fa5190e16d68ad864
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-09-27 06:05:52 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _memset - fill memory
+ * @s:Parameter
+ * @b:Parameter
+ * @n:Parameter
+ *
+ * Return:s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, 7);
+	return (s);
+}
