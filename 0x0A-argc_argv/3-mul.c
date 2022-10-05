bc881cb2e1929f0179d23edafebaadd49f6e08ff
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-10-05 07:22:45 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * main - main function
+ * @argc:parameter
+ * @argv:parameter
+ *
+ * Return: always 0
+ */
 int main(int argc, char *argv[])
 {
-	int res, num1 = 0, num2 = 0;
+	int res, firstnum = 0, secondnum = 0;
 
 	if (argc - 1 < 2)
 	{
@@ -13,9 +20,9 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		res = num1 * num2;
+		firstnum = atoi(argv[1]);
+		secondnum = atoi(argv[2]);
+		res = firstnum * secondnum;
 		printf("%d\n", res);
 	}
 	return (0);
