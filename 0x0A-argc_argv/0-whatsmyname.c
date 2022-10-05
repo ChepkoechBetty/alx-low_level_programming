3daf9b02a6629fa0544303ee5661e174ee375930
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-10-05 07:03:19 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,22 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * print_name - print name function
+ * @n:parameter
+ */
 void print_name(char *n)
 {
 	printf("%s\n", n);
 }
 
+/**
+ * main - main method
+ * @argc:parameter
+ * @argv:parameter
+ *
+ * Return: always 0
+ */
 int main(int argc, char *argv[])
 {
 	(void)argc;
