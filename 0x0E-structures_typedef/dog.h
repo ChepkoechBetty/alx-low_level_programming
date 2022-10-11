89e272224fe215e09637d281ce13dc40df979d96
ChepkoechBetty <chepkoechbetty2@gmail.com>
2022-10-11 09:30:40 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
+/**
+ * struct my_struct - Short description
+ * @name:first member
+ * @age:second member
+ * @ownere:third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
-}
+	char *owner;
+};
