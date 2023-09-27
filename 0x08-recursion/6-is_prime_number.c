5d0dd003d5756f730fb311fa7c3d2799b2c6470c
ChepkoechBetty <chepkoechbetty2@gmail.com>
2023-09-27 08:07:22 -0700

    isprimenumber?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,14 +6,18 @@
  */
 int is_prime_number(int n)
 {
-	if(n == 2)
+	if (n == 2)
 	{
-		return 1;
+		return (1);
 	}
-	if(n % 2 == 0)
+	if (n <= 1)
 	{
-		return 0;
+		return (0);
 	}
-	return 1;
+	if (n % 2 == 0)
+	{
+		return (0);
+	}
+	return (1);
 }
 
