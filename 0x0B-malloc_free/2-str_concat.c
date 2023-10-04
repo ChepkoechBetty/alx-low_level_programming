f1fb334ab80c8076fb5cbe8c9a41e6fcf6b81151
ChepkoechBetty <chepkoechbetty2@gmail.com>
2023-10-04 11:53:55 -0700

    strconcat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - Concatenate strings
+ * @s1:string one
+ * @s2:string two
+ * Return:pointer to newly allocated space
+ */
+char *str_concat(char *s1, char *s2)
+{
+	str=strcat(s1,s2);
+	char *ptr;
+
+	if(str ==NULL)
+	{
+		Return NULL;
+	
+}
